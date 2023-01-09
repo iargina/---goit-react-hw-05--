6b740578361a6984a7e73060ba346c7cb2fb038b
Irina <iargina.irina@gmail.com>
2023-01-09 23:32:18 +0200

    MovieCast update

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -2,6 +2,7 @@ import { gettingFilmCast } from 'services/filmDetails';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import css from './MovieCast.module.css';
+import image from 'images/photo_not_found.jpg';
 
 export const MovieCast = () => {
   const [actors, setActors] = useState([]);
@@ -11,7 +12,6 @@ export const MovieCast = () => {
   useEffect(() => {
     gettingFilmCast(movieId).then(response => setActors([...response]));
   }, [movieId]);
-  console.log(actors);
   return (
     actors.length > 0 && (
       <div className={css.actorsWrap}>
@@ -21,7 +21,7 @@ export const MovieCast = () => {
               {actor.profile_path ? (
                 <img src={actorPath + actor.profile_path} alt={actor.name} />
               ) : (
-                <img src="./images/photo_not_found.png" alt="no_photo" />
+                <img src={image} alt="no_photo" />
               )}
 
               {actor.character && (
